8d65d375ef19a1f30610569db55641455d75285f
guye <guye@geexfinance.com>
2019-12-30 13:33:50 +0800

    发起审批流程

--- webpack.config.js
+++ webpack.config.js
@@ -22,96 +22,98 @@ module.exports = {
         clientConfigEditor: './src/client/index.jsx',
         ulEditor: './src/editor/ul/index.jsx',
         scoreCardTable: './src/scorecard/index.jsx',
-        complexScoreCardTable: './src/complexscorecard/index.jsx',
         permissionConfigEditor: './src/permission/index.jsx'
     },
     output: {
         path: path.resolve('dist'),
-        filename: '[name].bundle.js'
+        filename: 'bundle/[name].bundle.js'
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            filename: 'index.html',
-            template: 'html/frame.html',
-            chunks: ["frame"]
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'variable-editor.html',
-            template: 'html/variable-editor.html',
-            chunks: ["variableEditor"]
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'ruleset-editor.html',
-            template: 'html/ruleset-editor.html',
-            chunks: ["ruleSetEditor"]
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'decision-table-editor.html',
-            template: 'html/decision-table-editor.html',
-            chunks: ["decisionTableEditor"]
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'decision-tree-editor.html',
-            template: 'html/decision-tree-editor.html',
-            chunks: ["decisionTreeEditor"]
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'score-card-editor.html',
-            template: 'html/score-card-editor.html',
-            chunks: ["scoreCardTable"]
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'complexscorecard-editor.html',
-            template: 'html/complexscorecard-editor.html',
-            chunks: ["complexScoreCardTable"]
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'rule-flow-designer.html',
-            template: 'html/rule-flow-designer.html',
-            chunks: ["flowDesigner"]
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'package-editor.html',
-            template: 'html/package-editor.html',
-            chunks: ["packageEditor"]
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'constant-editor.html',
-            template: 'html/constant-editor.html',
-            chunks: ["constantEditor"]
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'action-editor.html',
-            template: 'html/action-editor.html',
-            chunks: ["actionEditor"]
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'parameter-editor.html',
-            template: 'html/parameter-editor.html',
-            chunks: ["parameterEditor"]
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'script-decision-table-editor.html',
-            template: 'html/script-decision-table-editor.html',
-            chunks: ["scriptDecisionTableEditor"]
-        }),
-        new HtmlWebpackPlugin({
-            filename: 'ul-editor.html',
-            template: 'html/ul-editor.html',
-            chunks: ["ulEditor"]
-        }),
         // 复制文件
         new CopyWebpackPlugin([
             {
                 from: path.resolve(__dirname, 'lib'),
                 to: 'lib'
-                // ignore: ['.*']
             },
             {
                 from: path.resolve(__dirname, 'fonts'),
                 to: 'fonts'
+            },
+            {
+                from: path.resolve(__dirname, 'html/complexscorecard-editor.html'),
+                to: 'html/complexscorecard-editor.html'
             }
-        ])
+        ]),
+        new HtmlWebpackPlugin({
+            filename: 'html/index.html',
+            template: 'html/frame.html',
+            chunks: ["frame"]
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'html/variable-editor.html',
+            template: 'html/variable-editor.html',
+            chunks: ["variableEditor"]
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'html/ruleset-editor.html',
+            template: 'html/ruleset-editor.html',
+            chunks: ["ruleSetEditor"]
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'html/decision-table-editor.html',
+            template: 'html/decision-table-editor.html',
+            chunks: ["decisionTableEditor"]
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'html/decision-tree-editor.html',
+            template: 'html/decision-tree-editor.html',
+            chunks: ["decisionTreeEditor"]
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'html/score-card-editor.html',
+            template: 'html/score-card-editor.html',
+            chunks: ["scoreCardTable"]
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'html/rule-flow-designer.html',
+            template: 'html/rule-flow-designer.html',
+            chunks: ["flowDesigner"]
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'html/package-editor.html',
+            template: 'html/package-editor.html',
+            chunks: ["packageEditor"]
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'html/constant-editor.html',
+            template: 'html/constant-editor.html',
+            chunks: ["constantEditor"]
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'html/action-editor.html',
+            template: 'html/action-editor.html',
+            chunks: ["actionEditor"]
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'html/parameter-editor.html',
+            template: 'html/parameter-editor.html',
+            chunks: ["parameterEditor"]
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'html/script-decision-table-editor.html',
+            template: 'html/script-decision-table-editor.html',
+            chunks: ["scriptDecisionTableEditor"]
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'html/ul-editor.html',
+            template: 'html/ul-editor.html',
+            chunks: ["ulEditor"]
+        }),
+        new HtmlWebpackPlugin({
+            filename: 'html/permission-config-editor.html',
+            template: 'html/permission-config-editor.html',
+            chunks: ["permissionConfigEditor"]
+        })
     ],
     module: {
         rules: [
@@ -147,11 +149,11 @@ module.exports = {
         compress: true,
         port: 9000,
         proxy: {
-            '/frule/api/': {
+            '/urule/api/': {
                 target: 'http://127.0.0.1:8080/urule/',
                 changeOrigin: true,
                 pathRewrite: {
-                    '^/frule/api': ''
+                    '^/urule/api': ''
                 }
             }
         }
