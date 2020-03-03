de3f470b3abb387798946009c8c86ca7a0c32e0e
guye <guye@geexfinance.com>
2020-03-03 16:49:25 +0800

    最小宽度

--- src/components/splitter/component/Splitter.jsx
+++ src/components/splitter/component/Splitter.jsx
@@ -12,20 +12,22 @@ export default class Splitter extends Component{
         newPosition = Math.max(newPosition, this.minWidth);
         return newPosition;
     }
-    componentDidMount(){
-        const $domNode=$(ReactDOM.findDOMNode(this));
-        const $splitter=$domNode.split(this.props);
-        $(window).resize(function(){
+
+    componentDidMount() {
+        const $domNode = $(ReactDOM.findDOMNode(this));
+        const $splitter = $domNode.split(this.props);
+        $(window).resize(function() {
             var newPosition = this._sanitizeSplitterPositionValue($splitter.position());
             if (newPosition !== $splitter.position()) {
                 $splitter.position(newPosition);
             }
-            let height=$(window).height();
+            let height = $(window).height();
             $domNode.css({height:height});
         }.bind(this));
-        let height=$(window).height();
+        let height = $(window).height();
         $domNode.css({height:height});
     }
+
     render(){
         return (
             <div>
